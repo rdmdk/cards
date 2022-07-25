422768b812b765289bb0a16a01b0395f0c55ce88
Redmond <rdmdk@users.noreply.github.com>
2022-07-25 12:13:02 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -94,7 +94,7 @@ function game_over(a) {
 	
 	setTimeout(() => {
 		const h2 = m.querySelector('.winner h2');
-		h2.style.width = h2.clientWidth + 'px';
+		//h2.style.width = h2.clientWidth + 'px';
 		h2.innerHTML = '&nbsp;<em>winner!</em>';
 	}, 1500);
 }
