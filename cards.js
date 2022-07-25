0a49b2ca43586fa452f267abf4a1de26f5eaa4a3
Redmond <rdmdk@users.noreply.github.com>
2022-07-25 10:03:08 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,12 +1,13 @@
 const m = document.querySelector('main'),
-r = window.location.search ? Number(window.location.search.substring(1)) : Math.random() * (12 - 2) + 2,
 clubs = ['c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck', 'ca'],
 diamonds = ['d2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk', 'da'],
 hearts = ['h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk', 'ha'],
 spades = ['s2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk', 'sa'];
 
 let deck = [...clubs, ...diamonds, ...hearts, ...spades].sort(() => Math.random() - 0.5),
-players, buttons;
+r = window.location.search ? Number(window.location.search.substring(1)) : Math.random() * (12 - 2) + 2,
+players,
+buttons;
 
 r = r < 2 ? 2 : r > 12 ? 12 : r;
 
