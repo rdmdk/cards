77d8931b11c193ecf85d7fabe6de1912f70646f6
Redmond <rdmdk@users.noreply.github.com>
2022-07-30 17:00:28 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -62,7 +62,7 @@ function stand(a) {
 }
 
 function blackjack(a) {
-  if (a.querySelector('.card[class$="7"]').length === 3) a.classList.add('payday');
+  if (a.querySelectorAll('.card[class$="7"]').length === 3) a.classList.add('payday');
   a.classList.add('blackjack', 'done');
   a.querySelector('h2').innerHTML = '&nbsp;<em>blackjack!</em>';
 }
