88597d3ea09ca239faaa16c116ae668def05ae5a
Redmond <rdmdk@users.noreply.github.com>
2022-08-05 09:46:22 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -99,7 +99,7 @@ function bust(a) {
 function bet(a) {
   const banks = localStorage.banks ? localStorage.banks.split(',') : Array(r).fill(100),
         bank = Number(banks[[...players].indexOf(a)]),
-        betting = Math.floor((Math.random() * (bank - 5) + 5) / 5) * 5 : bank <= 10 ? 5 : 5;
+        betting = bank <= 10 ? 5 : Math.floor((Math.random() * (bank - 5) + 5) / 5) * 5;
             
   a.querySelector('.bank').innerText = bank;
   a.querySelector('.bet').innerText = betting;
