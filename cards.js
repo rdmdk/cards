7b257338ca97e11b1d10071c5f48c69a44b45e90
Redmond <rdmdk@users.noreply.github.com>
2023-01-16 14:56:51 +0000

    Update cards.js

--- cards.js
+++ cards.js
@@ -383,6 +383,4 @@ setTimeout(() => {
 	}
 }, 4e3);
 
-if ('serviceWorker' in navigator) 
-navigator.serviceWorker.register('service-worker.js');
-
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('service-worker.js');
