db161417a20f386efd21b89d8f8614641abba1ef
Redmond <rdmdk@users.noreply.github.com>
2023-08-13 15:31:47 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -201,8 +201,8 @@ function next_turn() {
 		setTimeout(() => {
 			const a_p = m.querySelector('.active');
 			const a_o = a_p.offsetTop + a_p.querySelector('.actions').offsetTop;
-			if (a_o >= m.scrollTop + window.innerHeight)) m.scrollTo(0, a_p.offsetTop);
-			else if (a_o < m.scrollTop - window.innerHeight)) m.scrollTo(0, 0);
+			if (a_o >= m.scrollTop + window.innerHeight) m.scrollTo(0, a_p.offsetTop);
+			else if (a_o < m.scrollTop - window.innerHeight) m.scrollTo(0, 0);
 		}, 500);
 	}, to);
 
